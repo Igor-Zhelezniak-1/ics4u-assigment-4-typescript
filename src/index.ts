0026feb5bf1d5a90218b58a02c206dbc25c29435
Igor-Zhelezniak-1 <90275833+Igor-Zhelezniak-1@users.noreply.github.com>
2023-01-18 02:22:12 -0500

    -

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+/**
+ * The program
+ *
+ *
+ * By: Igor
+ * Version: 1.0
+ * Since:   2022-09-17
+ */
+
+import promptSync from 'prompt-sync'
+const prompt = promptSync()
+
+function asterisk(integer: number, spacesNum: number = 0) {
+  // Generate asterisk.
+  let output: string = ''
+
+  for (let counter: number = 0; counter < spacesNum; counter++) {
+    output += ' '
+  }
+
+  for (let counter1: number = 0; counter1 < integer; counter1++) {
+    output += '* '
+  }
+
+  console.log(output)
+
+  if (integer > 1) {
+    asterisk(integer - 1, spacesNum + 1)
+  }
+  console.log(output)
+}
+
+const input = prompt('Insert height of hourglass: ')
+try {
+  const inputNum = parseInt(input)
+
+  if (isNaN(inputNum)) {
+    console.log('This is not a number.')
+  } else if (inputNum <= 0) {
+    console.log('This is not an integer')
+  } else asterisk(inputNum)
+} catch (e) {
+  console.log('\nThis is not a number')
+}
+
+console.log('Done.')
